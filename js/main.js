5a174d15aa9394fe5a69fc4f4a62466da3c5d524
Jaloliddin-Zokirov <jaloliddinzokirov1908@gmail.com>
2022-07-05 19:34:56 +0500

    Add new teg

--- js/main.js
+++ js/main.js
@@ -1,73 +1,106 @@
-let elList = document.querySelector('.js-list')
+let elList = document.querySelector('.js-list');
 
-const date = function(a) {
-  return (Math.round(a / 31536000) + 1970);
-}
+const date = function (a) {
+	return Math.round(a / 31536000) + 1970;
+};
 
 function appendToDom(array, node) {
-  for (item of array) {
-    let newItem = document.createElement("li");
-    let newBox = document.createElement("div");
-    let elTitle = document.createElement("h2");
-    let elId = document.createElement("span");
-    let elImg = document.createElement("img");
-    let elGenres = document.createElement("p");
-    let elOverview = document.createElement("p");
-    let data = document.createElement("time");
-    elTitle.textContent = `${item.title}`;
-    elId.textContent = `${item.id}`;
-    elImg.src = `${item.poster}`;
-    elGenres.textContent = `action: ${item.genres}`;
-    elOverview.textContent = `${item.overview}`;
-    data.textContent = `Release date of the film: ${date(item.release_date)}`;
-  
-    elImg.setAttribute('class','img')
-    elTitle.setAttribute('class', 'subject')
-    elId.setAttribute('class', 'id')
-    elGenres.setAttribute('class', 'gener')
-    elOverview.setAttribute('class', 'over')
-    data.setAttribute('class', 'data')
-    newBox.setAttribute('class', 'box')
-    newItem.setAttribute('class','item')
-  
-    newItem.appendChild(elImg)
-    newBox.appendChild(elTitle)
-    newBox.appendChild(elId)
-    newBox.appendChild(elGenres)
-    newBox.appendChild(elOverview)
-    newBox.appendChild(data)
-    newItem.appendChild(newBox)
-    node.appendChild(newItem);
-  }
+	for (item of array) {
+		let newItem = document.createElement('li');
+		let newBox = document.createElement('div');
+		let elTitle = document.createElement('h2');
+		let elId = document.createElement('span');
+		let elImg = document.createElement('img');
+		let elGenres = document.createElement('p');
+		let elOverview = document.createElement('p');
+		let data = document.createElement('time');
+		elTitle.textContent = `${item.title}`;
+		elId.textContent = `${item.id}`;
+		elImg.src = `${item.poster}`;
+		elGenres.textContent = `action: ${item.genres}`;
+		elOverview.textContent = `${item.overview}`;
+		data.textContent = `Release date of the film: ${date(item.release_date)}`;
+
+		elImg.setAttribute('class', 'img');
+		elTitle.setAttribute('class', 'subject');
+		elId.setAttribute('class', 'id');
+		elGenres.setAttribute('class', 'gener');
+		elOverview.setAttribute('class', 'over');
+		data.setAttribute('class', 'data');
+		newBox.setAttribute('class', 'box');
+		newItem.setAttribute('class', 'item');
+
+		newItem.appendChild(elImg);
+		newBox.appendChild(elTitle);
+		newBox.appendChild(elId);
+		newBox.appendChild(elGenres);
+		newBox.appendChild(elOverview);
+		newBox.appendChild(data);
+		newItem.appendChild(newBox);
+		node.appendChild(newItem);
+	}
 }
 
 appendToDom(films, elList);
 
-let elSelect = document.querySelector(".js-select");
+
+
+let elAz = document.querySelector('.js-a-z');
+
+elAz.addEventListener('click', () => {
+	elList.innerHTML = '';
+
+	let newAz = films.sort((a, b) => {
+		if (a.title > b.title) return 1;
+		return -1;
+	});
+
+	appendToDom(newAz, elList);
+});
+
+
+
+let elZa = document.querySelector('.js-z-a');
+
+elZa.addEventListener('click', () => {
+	elList.innerHTML = '';
+
+	let newZa = films.sort((a, b) => {
+		if (a.title < b.title) return 1;
+		return -1;
+	});
+
+	appendToDom(newZa, elList);
+});
+
+
+
+
+let elSelect = document.querySelector('.js-select');
 
 let result = [];
-elSelect.addEventListener("change", () => {
-  result = [];
-  elList.innerHTML = "";
-  let elSelectVal = elSelect.value;
-  films.forEach((poc) => {
-    if (poc.genres.includes(elSelectVal)) {
-      result.push(poc);
-    }
-  });
-  appendToDom(result, elList);
+elSelect.addEventListener('change', () => {
+	result = [];
+	elList.innerHTML = '';
+	let elSelectVal = elSelect.value;
+	films.forEach((poc) => {
+		if (poc.genres.includes(elSelectVal)) {
+			result.push(poc);
+		}
+	});
+	appendToDom(result, elList);
 });
 
 let optionList = new Set();
 
 films.forEach((item) => {
-  optionList.add(...item.genres);
+	optionList.add(...item.genres);
 });
 
 optionList.forEach((elment) => {
-  let newOption = document.createElement("option");
-  newOption.textContent = elment;
-  newOption.value = elment;
-  
-  elSelect.appendChild(newOption)
+	let newOption = document.createElement('option');
+	newOption.textContent = elment;
+	newOption.value = elment;
+
+	elSelect.appendChild(newOption);
 });
